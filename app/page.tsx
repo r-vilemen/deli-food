8e14ba004ccbbaa39fd88d572feb0f4479d3c71c
r-vilemen <rodrigo.vilemen@hotmail.com>
2024-05-07 18:11:58 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "./src/components/header";
+import Search from "./src/components/search";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="pt6 px-5">
+        <Search />
+      </div>
+    </>
+  );
 };
 
 export default Home;
