1b6e521b7d1f2a81184bcfd16468b57fc99eeab2
r-vilemen <rodrigo.vilemen@hotmail.com>
2024-05-07 21:46:21 -0300

    feat: add category item and list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "./src/components/category/category-list";
 import Header from "./src/components/header";
 import Search from "./src/components/search";
 
@@ -7,6 +8,10 @@ const Home = () => {
       <Header />
       <div className="pt6 px-5">
         <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
       </div>
     </>
   );
